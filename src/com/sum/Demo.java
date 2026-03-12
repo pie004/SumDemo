7c783e049a059e847eaf6f710ea3938eefc88870
pie004 <piyasad78@gmail.com>
2026-03-12 10:32:17 +0530

    made some changes  in code

--- src/com/sum/Demo.java
+++ src/com/sum/Demo.java
@@ -2,4 +2,12 @@ package com.sum;
 
 public class Demo {
 
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
 }
